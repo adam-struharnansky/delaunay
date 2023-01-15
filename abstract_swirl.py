33d574ccf42838984d239e23bde8e03298e55848
Martin Halaj <mhalaj999@gmail.com>
2023-01-15 11:15:21 +0100

    Abstract swirl and Rythme attempt

---
+++ abstract_swirl.py
@@ -0,0 +1,61 @@
+import colors as c
+import turtle
+import random
+from myTurtle_A import *
+
+previous = []
+
+
+def generate_color(step):
+    col = c.any_swirl()
+    if step == 0:
+        while col == c.swirl_white():
+            col = c.any_swirl()
+    return col
+
+
+def draw_circle(t: MyTurtle_A, radius: int, angle: int, step: int):
+    col = generate_color(step)
+    while col in previous:
+        col = generate_color(step)
+    previous.append(col)
+
+    t.color(col)
+    t.begin_fill()
+    t.circle(radius, angle)
+    pos = t.position()
+    head = t.heading()
+    t.left(90)
+    t.fd(radius)
+    t.end_fill()
+    t.jump_to(pos)
+    t.setheading(head)
+
+    col = generate_color(step)
+    while col in previous:
+        col = generate_color(step)
+    previous.append(col)
+
+    t.color(col)
+    t.begin_fill()
+    t.circle(radius, 360 - angle)
+    pos = t.position()
+    head = t.heading()
+    t.left(90)
+    t.fd(radius)
+    t.end_fill()
+    t.jump_to(pos)
+    t.setheading(head)
+
+
+def abstract_swirl(t: MyTurtle_A, radius: int):
+    global previous
+    turtle.bgcolor(c.swirl_white())
+    for i in range(10):
+        t.jump_to(0, 0)
+        t.setheading(180)
+        t.jump_by(0, radius)
+        draw_circle(t, radius, random.randint(60, 300), i)
+        radius -= random.randint(10, 35)
+        if i > 0:
+            previous = previous[-2:]
---
+++ rythme.py
@@ -0,0 +1,93 @@
+import random
+
+from myTurtle_A import *
+import colors as c
+import turtle
+
+three = []
+coordinates = [(0, 280), (0, 0), (0, -280)]
+
+
+def draw_circle(t: MyTurtle_A, radius: int, angle: int):
+    t.begin_fill()
+    t.circle(radius, angle)
+    h = t.heading()
+    pos = t.position()
+    t.left(90)
+    t.fd(radius)
+    t.end_fill()
+    t.setheading(h)
+    t.jump_to(pos)
+
+
+def background(t: MyTurtle_A, bg_radius):
+    t.setheading(180)
+    t.jump_to(0, bg_radius + 50)
+    top = 400
+    radius = bg_radius
+    for i in range(8):
+        t.color(c.random_hsv())
+        draw_circle(t, radius, 180)
+        change = random.randint(5, 40)
+        radius -= change
+        top -= change
+        t.jump_to(0, top)
+        t.setheading(180)
+
+    radius = bg_radius
+    t.setheading(0)
+    t.jump_to(0, -(bg_radius + 50))
+    top = -400
+    for i in range(8):
+        t.color(c.random_hsv())
+        draw_circle(t, radius, 180)
+        change = random.randint(5, 40)
+        radius -= change
+        top += change
+        t.jump_to(0, top)
+        t.setheading(0)
+
+
+def top_three(t: MyTurtle_A):
+    t.jump_to(0, 0)
+    for i in range(28):
+        area = random.randint(0, 2)
+        t.jump_to(coordinates[area])
+        whole = random.randint(0, 1)
+        t.setheading(180)
+        if whole == 1:
+            t.jump_by(0, three[area])
+            t.color(c.random_hsv())
+            draw_circle(t, three[area], 360)
+
+        else:
+            two_halfs = random.randint(0, 1)
+            if two_halfs == 1:
+                t.jump_by(0, three[area])
+                t.color(c.random_hsv())
+                draw_circle(t, three[area], 180)
+                t.color(c.random_hsv())
+                draw_circle(t, three[area], 180)
+
+            else:
+                right = 1
+                if right == 0:
+                    t.jump_by(0, three[area])
+                    t.color(c.random_hsv())
+                    draw_circle(t, three[area], 180)
+
+                else:
+                    t.setheading(0)
+                    t.jump_by(0, -three[area])
+                    t.color(c.random_hsv())
+                    draw_circle(t, three[area], 180)
+
+        change = random.randint(5, 22)
+        three[area] -= change
+
+
+def rythme(t: MyTurtle_A, bg_radius, top_radius):
+    background(t, bg_radius)
+    for i in range(3):
+        three.append(top_radius)
+    top_three(t)
